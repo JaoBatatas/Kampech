a35960cc71896e83dd6c15ec32f7ea56bcdc0818
João Paulo Ersching <joao.ersching@gmail.com>
2023-04-19 09:41:02 -0300

    js brabo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function show() {
     const stylesheet = document.styleSheets[0];
     let elementRules;
+    // let searchBar = document.querySelector(".searchBar")
 
     for (let i = 0; i < stylesheet.cssRules.length; i++) {
         if (stylesheet.cssRules[i].selectorText === '#searchBar') {
@@ -12,6 +13,20 @@ function show() {
         elementRules.style.display = 'block';
     }
     else {
-        elementRules.style.display = 'none';
+        if (searchBar.value == '') {
+            alert("Campo vazio");
+        }
+        else{
+            alert("Buscnado");
+        }
     }
 }
+
+
+
+
+// if (email_box.value == '') {
+//     alert ("Campo vazio");
+// } else {
+//     alert("Redirecionando")
+// }
