764986b7f50e2e931c12788b30cd8d90ad9f44d7
João Paulo Ersching <joao.ersching@gmail.com>
2023-04-19 08:18:14 -0300

    start register page

---
+++ index.js
@@ -0,0 +1,17 @@
+function show() {
+    const stylesheet = document.styleSheets[0];
+    let elementRules;
+
+    for (let i = 0; i < stylesheet.cssRules.length; i++) {
+        if (stylesheet.cssRules[i].selectorText === '#searchBar') {
+            elementRules = stylesheet.cssRules[i];
+        }
+    }
+
+    if (elementRules.style.display === 'none') {
+        elementRules.style.display = 'block';
+    }
+    else {
+        elementRules.style.display = 'none';
+    }
+}
