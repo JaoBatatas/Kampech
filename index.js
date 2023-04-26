9fc01fe5fe52240d0f3cad524b44d9348a6c924f
João Paulo Ersching <joao.ersching@gmail.com>
2023-04-26 08:56:01 -0300

    algo foi feito - 26/04

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function show() {
         if (searchBar.value == '') {
             alert("Campo vazio");
         }
-        else{
+        else {
             alert("Buscnado");
         }
     }
@@ -24,12 +24,12 @@ function show() {
 const handlePhone = (event) => {
     let input = event.target
     input.value = phoneMask(input.value)
-  }
-  
-  const phoneMask = (value) => {
+}
+
+const phoneMask = (value) => {
     if (!value) return ""
-    value = value.replace(/\D/g,'')
-    value = value.replace(/(\d{2})(\d)/,"($1) $2")
-    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    value = value.replace(/\D/g, '')
+    value = value.replace(/(\d{2})(\d)/, "($1) $2")
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2")
     return value
-  }
+}
