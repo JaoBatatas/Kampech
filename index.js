ab702083b99a04420066b01bcbf014890ad3f345
João Paulo Ersching <joao.ersching@gmail.com>
2023-04-28 09:17:35 -0300

    start of itens list and js to change quantity

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Search Bar Animation
 function searchBar_slide() {
     const stylesheet = document.styleSheets[0];
     let elementRules;
@@ -21,6 +23,7 @@ function searchBar_slide() {
     }
 }
 
+// Cart PopUp Animation
 function cartAnimation() {
     const stylesheet = document.styleSheets[0];
     let elementRules;
@@ -32,14 +35,14 @@ function cartAnimation() {
     }
 
     if (elementRules.style.display === 'none') {
-        elementRules.style.display = 'block';
+        elementRules.style.display = 'flex';
     }
     else {
        elementRules.style.display = 'none';
     }
 }
 
-
+// Phone Number Mask
 const handlePhone = (event) => {
     let input = event.target
     input.value = phoneMask(input.value)
@@ -52,3 +55,25 @@ const phoneMask = (value) => {
     value = value.replace(/(\d)(\d{4})$/, "$1-$2")
     return value
 }
+
+// Change Itens Quantity
+const quantityGroupClass = "CLASS-NAME-HERE"
+const quantityIncrementButtonClass = "CLASS-NAME-HERE"
+const quantityDecrementButtonClass = "CLASS-NAME-HERE"
+const quantityNumberFieldClass = "CLASS-NAME-HERE"
+
+// Increment
+$(document).on('click', `.${quantityIncrementButtonClass}`, function(){
+  var $input = $(this).closest(`.${quantityGroupClass}`).find(`.${quantityNumberFieldClass}`);    
+  var val = parseInt($input.val(), 10);
+  $input.val(val + 1);
+  $input[0].dispatchEvent(new Event('change'));
+});
+
+// Decrement
+$(document).on('click', `.${quantityDecrementButtonClass}`, function(){
+  var $input = $(this).closest(`.${quantityGroupClass}`).find(`.${quantityNumberFieldClass}`);    
+  var val = parseInt($input.val(), 10);
+  $input.val(Math.max(val - 1, 1));
+  $input[0].dispatchEvent(new Event('change'));
+});
