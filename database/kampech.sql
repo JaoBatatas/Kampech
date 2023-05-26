06f1b41f93cfea13370a47ff67cc34121685bfb4
João Paulo Ersching <joao.ersching@gmail.com>
2023-05-26 08:58:49 -0300

    Fix raw database

--- database/kampech.sql
+++ database/kampech.sql
@@ -1,257 +1,139 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 19-Maio-2023 às 14:20
--- Versão do servidor: 8.0.21
--- versão do PHP: 8.1.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `kampech`
---
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `address`
---
-
-CREATE TABLE `address` (
+CREATE TABLE `kp_address` (
   `id_address` int NOT NULL,
   `id_user` int NOT NULL,
   `address` varchar(500) NOT NULL,
-  `zip_code` varchar(20) NOT NULL,
+  `zip_code` varchar(8) NOT NULL,
   `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL
+  `state` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cart`
---
-
-CREATE TABLE `cart` (
+CREATE TABLE `kp_cart` (
   `id_cart` int NOT NULL,
   `id_user` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `cart_items`
---
-
-CREATE TABLE `cart_items` (
+CREATE TABLE `kp_cart_items` (
   `id_cart_items` int NOT NULL,
   `id_cart` int NOT NULL,
   `id_product` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `order`
---
-
-CREATE TABLE `order` (
+CREATE TABLE `kp_order` (
   `id_order` int NOT NULL,
   `id_user` int NOT NULL,
-  `date` date NOT NULL,
+  `order_date` date NOT NULL,
   `total` decimal(10,2) NOT NULL,
-  `payment` varchar(300) NOT NULL
+  `payment` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `order_items`
---
-
-CREATE TABLE `order_items` (
+CREATE TABLE `kp_order_items` (
   `id_order_items` int NOT NULL,
   `id_order` int NOT NULL,
   `id_product` int NOT NULL,
-  `price` int NOT NULL
+  `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `product`
---
-
-CREATE TABLE `product` (
+CREATE TABLE `kp_product` (
   `id_product` int NOT NULL,
-  `name` varchar(500) NOT NULL,
-  `description` varchar(2000) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `description` varchar(1000) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `image_url` varchar(500) NOT NULL,
-  `layout` varchar(20) NOT NULL,
-  `size` varchar(20) NOT NULL,
-  `connection` varchar(20) NOT NULL
+  `image_url` varchar(1000) NOT NULL,
+  `layout` varchar(100) NOT NULL,
+  `size` varchar(10) NOT NULL,
+  `connection` varchar(100) NOT NULL,
+  `switch` varchar(100) NOT NULL,
+  `main_color` varchar(100) NOT NULL,
+  `key_color` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `user`
---
-
-CREATE TABLE `user` (
+CREATE TABLE `kp_user` (
   `id_user` int NOT NULL,
   `name` varchar(300) NOT NULL,
   `email` varchar(300) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `cpf` varchar(20) NOT NULL,
-  `phone` varchar(20) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `cpf` varchar(11) NOT NULL,
+  `phone` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `address`
---
-ALTER TABLE `address`
+ALTER TABLE `kp_address`
   ADD PRIMARY KEY (`id_address`),
-  ADD KEY `fk_user` (`id_user`);
+  ADD KEY `address_fkUser` (`id_user`);
 
---
--- Índices para tabela `cart`
---
-ALTER TABLE `cart`
+ALTER TABLE `kp_cart`
   ADD PRIMARY KEY (`id_cart`),
-  ADD KEY `fk_user3` (`id_user`);
+  ADD KEY `cart_fkUser` (`id_user`);
 
---
--- Índices para tabela `cart_items`
---
-ALTER TABLE `cart_items`
+ALTER TABLE `kp_cart_items`
   ADD PRIMARY KEY (`id_cart_items`),
-  ADD KEY `fk_cart` (`id_cart`),
-  ADD KEY `fk_product2` (`id_product`);
+  ADD KEY `cart_items_fkCart` (`id_cart`),
+  ADD KEY `cart_items_fkProduct` (`id_product`);
 
---
--- Índices para tabela `order`
---
-ALTER TABLE `order`
+ALTER TABLE `kp_order`
   ADD PRIMARY KEY (`id_order`),
-  ADD KEY `fk_user2` (`id_user`);
+  ADD KEY `order_fkUser` (`id_user`);
 
---
--- Índices para tabela `order_items`
---
-ALTER TABLE `order_items`
+ALTER TABLE `kp_order_items`
   ADD PRIMARY KEY (`id_order_items`),
-  ADD KEY `fk_order` (`id_order`),
-  ADD KEY `fk_product` (`id_product`);
+  ADD KEY `order_items_fkOrder` (`id_order`),
+  ADD KEY `order_items_fkProduct` (`id_product`);
 
---
--- Índices para tabela `product`
---
-ALTER TABLE `product`
+ALTER TABLE `kp_product`
   ADD PRIMARY KEY (`id_product`);
 
---
--- Índices para tabela `user`
---
-ALTER TABLE `user`
+ALTER TABLE `kp_user`
   ADD PRIMARY KEY (`id_user`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `address`
---
-ALTER TABLE `address`
-  MODIFY `id_address` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+ALTER TABLE `kp_address`
+  MODIFY `id_address` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `cart`
---
-ALTER TABLE `cart`
-  MODIFY `id_cart` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `kp_cart`
+  MODIFY `id_cart` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `cart_items`
---
-ALTER TABLE `cart_items`
-  MODIFY `id_cart_items` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `kp_cart_items`
+  MODIFY `id_cart_items` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `order`
---
-ALTER TABLE `order`
-  MODIFY `id_order` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+ALTER TABLE `kp_order`
+  MODIFY `id_order` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `order_items`
---
-ALTER TABLE `order_items`
-  MODIFY `id_order_items` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+ALTER TABLE `kp_order_items`
+  MODIFY `id_order_items` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `product`
---
-ALTER TABLE `product`
-  MODIFY `id_product` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+ALTER TABLE `kp_product`
+  MODIFY `id_product` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT de tabela `user`
---
-ALTER TABLE `user`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+ALTER TABLE `kp_user`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
 
---
--- Restrições para despejos de tabelas
---
 
---
--- Limitadores para a tabela `address`
---
-ALTER TABLE `address`
-  ADD CONSTRAINT `fk_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+ALTER TABLE `kp_address`
+  ADD CONSTRAINT `address_fkUser` FOREIGN KEY (`id_user`) REFERENCES `kp_user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
---
--- Limitadores para a tabela `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `fk_user3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+ALTER TABLE `kp_cart`
+  ADD CONSTRAINT `cart_fkUser` FOREIGN KEY (`id_user`) REFERENCES `kp_user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
---
--- Limitadores para a tabela `cart_items`
---
-ALTER TABLE `cart_items`
-  ADD CONSTRAINT `fk_cart` FOREIGN KEY (`id_cart`) REFERENCES `cart` (`id_cart`),
-  ADD CONSTRAINT `fk_product2` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`);
+ALTER TABLE `kp_cart_items`
+  ADD CONSTRAINT `cart_items_fkCart` FOREIGN KEY (`id_cart`) REFERENCES `kp_cart` (`id_cart`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `cart_items_fkProduct` FOREIGN KEY (`id_product`) REFERENCES `kp_product` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
---
--- Limitadores para a tabela `order`
---
-ALTER TABLE `order`
-  ADD CONSTRAINT `fk_user2` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+ALTER TABLE `kp_order`
+  ADD CONSTRAINT `order_fkUser` FOREIGN KEY (`id_user`) REFERENCES `kp_user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
---
--- Limitadores para a tabela `order_items`
---
-ALTER TABLE `order_items`
-  ADD CONSTRAINT `fk_order` FOREIGN KEY (`id_order`) REFERENCES `order` (`id_order`),
-  ADD CONSTRAINT `fk_product` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`);
+ALTER TABLE `kp_order_items`
+  ADD CONSTRAINT `order_items_fkOrder` FOREIGN KEY (`id_order`) REFERENCES `kp_order` (`id_order`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `order_items_fkProduct` FOREIGN KEY (`id_product`) REFERENCES `kp_product` (`id_product`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
