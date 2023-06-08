d4e006fd01ea30e04b46c4c424223ca1c2ddbd1b
João Paulo Ersching <joao.ersching@gmail.com>
2023-06-07 21:16:58 -0300

    Update server and database

--- server.js
+++ server.js
@@ -1,55 +1,52 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mysql = require('mysql2');
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2/promise'); // Importe o pacote mysql2/promise para usar o método createPool
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static('css'))
-app.use(express.static('assets'))
-app.use(express.static('html'))
-app.use(express.static('js'))
+const app = express();
 
-const connection = mysql.createConnection({
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('css'));
+app.use(express.static('assets'));
+app.use(express.static('html'));
+app.use(express.static('js'));
+
+const pool = mysql.createPool({ // Use createPool em vez de createConnection
   host: '127.0.0.1',
   user: 'root',
   password: 'root',
   database: 'kampech'
 });
 
-connection.connect(function (err) {
-  console.log("Conexão como o Banco realizada com sucesso!")
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
-
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => { // Use async/await para tornar a função assíncrona
   let email = req.body.email;
   let password = req.body.password;
 
-  connection.query(`SELECT * FROM kp_user where email = '${email}'`, function (err, rows, fields) {
-    if (!err) {
-      console.log("Resultado:", rows);
+  try {
+    const connection = await pool.getConnection(); // Obtenha uma conexão da piscina
 
-      if (rows.length === 0) {
-        res.redirect('/login.html?404account');
-      } else {
+    const [rows, fields] = await connection.execute(`SELECT * FROM kp_user WHERE email = ?`, [email]); // Use prepared statements para evitar ataques de SQL Injection
 
-        if (password === rows[0].password) {
-          // res.send('Login realizado com sucesso!');
-          res.redirect('/personalInfo.html');
-        }
-        else {
-          res.redirect('/login.html?passwordError');
-        }
-      }
+    connection.release(); // Libere a conexão de volta para a piscina
+
+    if (rows.length === 0) {
+      res.redirect('/login.html?404account');
     } else {
-      console.log("Erro: Consulta não realizada", err);
+      if (password === rows[0].password) {
+        res.redirect('/personalInfo.html');
+      } else {
+        res.redirect('/login.html?passwordError');
+      }
     }
-  });
-})
+  } catch (error) {
+    console.log('Erro: Consulta não realizada', error);
+  }
+});
+
 app.listen(3700, () => {
-  console.log('Servidor rodando na porta 3700!')
-})
+  console.log('Servidor rodando na porta 3700!');
+});
