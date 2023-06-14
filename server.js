be9f4e79bbaf7799efff139a32f5bca4fd43849d
João Paulo Ersching <joao.ersching@gmail.com>
2023-06-14 08:52:35 -0300

    Edit server.js

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const oneDay = 1000 * 60 * 60 * 24;
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
+const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql2');
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.static('css'))
-app.use(express.static('assets'))
-app.use(express.static('html'))
-app.use(express.static('js'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('css'));
+app.use(express.static('assets'));
+app.use(express.static('html'));
+app.use(express.static('js'));
+app.use(sessions({
+  secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
+  saveUninitialized: true,
+  cookie: { maxAge: oneDay },
+  resave: false
+}));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: '127.0.0.1',
@@ -21,9 +33,15 @@ connection.connect(function (err) {
   console.log("Conexão como o Banco realizada com sucesso!!!")
 });
 
+let session;
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+  session = req.session;
+  if (session.id_user) {
+    res.send("Welcome User <a href=\'/logout'>click to logout</a>");
+  } else
+    res.sendFile('views/index.html', { root: __dirname })
+});
 
 app.post('/login', (req, res) => {
   let email = req.body.email;
@@ -36,19 +54,65 @@ app.post('/login', (req, res) => {
       if (rows.length === 0) {
         res.redirect('/login.html?404account');
       } else {
-
         if (password === rows[0].password) {
-          res.send('Login realizado com sucesso!');
+          session = req.session;
+          session.id_user = req.body.email;
+          console.log(req.session)
+          res.redirect('/personalInfo.html');
         }
         else {
-          res.send('Senha incorreta');
+          res.redirect('/login.html?passwordError');
         }
       }
     } else {
       console.log("Erro: Consulta não realizada", err);
     }
   });
-})
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+app.get('/getPersonalInfo', (req, res) => {
+  // Verificar se o usuário está autenticado
+  if (!req.session.id_user) {
+    res.status(401).json({ error: 'Usuário não autenticado' });
+    return;
+  }
+
+  // Obter as informações pessoais do usuário no banco de dados
+  const email = req.session.id_user;
+
+  connection.query(`SELECT kp_user.*, kp_address.* FROM kp_user
+    LEFT JOIN kp_address ON kp_user.id_user = kp_address.id_user
+    WHERE kp_user.email = '${email}'`, (err, rows, fields) => {
+    if (!err) {
+      if (rows.length === 0) {
+        res.status(404).json({ error: 'Usuário não encontrado' });
+      } else {
+        // Enviar as informações pessoais do usuário como resposta JSON
+        const personalInfo = {
+          name: rows[0].name,
+          address: rows[0].address,
+          cpf: rows[0].cpf,
+          zip_code: rows[0].zip_code,
+          city: rows[0].city,
+          state: rows[0].state,
+          phone: rows[0].phone
+        };
+
+        res.json(personalInfo);
+      }
+    } else {
+      console.log("Erro: Consulta não realizada", err);
+      res.status(500).json({ error: 'Erro no servidor' });
+    }
+  });
+});
+
+
 app.listen(3700, () => {
   console.log('Servidor rodando na porta 3700!')
-})
+});
